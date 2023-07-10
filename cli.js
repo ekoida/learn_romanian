571cfcf17fbfcefa470208be6649e3824e5e8055
ekoida <celeno4ka@gmail.com>
2023-07-10 23:05:18 +0300

    added simple slider

---
+++ cli.js
@@ -0,0 +1,25 @@
+import { resolve } from "path"
+import { readdir, stat, writeFile } from "node:fs/promises"
+
+
+
+
+const result = []
+async function createSrc(dir) {
+
+  const entries = await readdir(resolve(dir))
+  for (const entry of entries) {
+    const s = await stat(resolve(`${dir}/${entry}`))
+    if (s.isDirectory()) {
+      await createSrc(`${dir}/${entry}`)
+    } else {
+      result.push(`${dir}/${entry}`)
+    }
+  }
+}
+
+await createSrc('img')
+
+await writeFile('photo.js', `const list=${JSON.stringify(result, null, 2)};`)
+
+console.log(result)
--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-const button = document.querySelector('button')
-button.addEventListener('click', () => {
-  alert("hello world");
+let viewed = 0
+
+const next = document.querySelector('.next')
+const prev = document.querySelector('.prev')
+const figure = document.querySelector('.image')
+
+const img = figure.querySelector('img')
+
+img.src = list[0];
+
+
+
+next.addEventListener('click', () => {
+
+  img.src = list[viewed + 1]
+  viewed = viewed + 1
+})
+
+prev.addEventListener('click', () => {
+  if (viewed < 0) {
+    viewed = 1
+  }
+  img.src = list[viewed - 1]
+  viewed = viewed - 1
 })
