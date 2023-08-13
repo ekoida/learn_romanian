4c268c77b9c6c72a4af37acb40d14e21cef610be
ekoida <celeno4ka@gmail.com>
2023-08-13 17:55:32 +0300

    started filters implementation

--- script.js
+++ script.js
@@ -1,39 +1,53 @@
-import { list } from "./photo.js"
+import { list } from "./photo.js";
 
-let viewed = 0
+let viewed = 0;
 
-const next = document.querySelector('.next')
-const prev = document.querySelector('.prev')
-const figure = document.querySelector('.image')
-const img = figure.querySelector('img')
-const description = figure.querySelector(".description")
+const filterControl = document.querySelector(".filter-control");
+const menu = document.querySelector(".menu");
+const next = document.querySelector(".next");
+const prev = document.querySelector(".prev");
+const figure = document.querySelector(".image");
+const img = figure.querySelector("img");
+const description = figure.querySelector(".description");
 
-const audio = document.querySelector('audio')
+const audio = document.querySelector("audio");
 
+filterControl.addEventListener("click", () => {
+  menu.classList.toggle("show");
+});
 
 img.src = list[0];
 
-next.addEventListener('mousedown', () => {
-  audio.play()
-})
-prev.addEventListener('mousedown', () => {
-  audio.play()
-})
+next.addEventListener("mousedown", () => {
+  audio.play();
+});
 
-next.addEventListener('click', () => {
-  const nextImage = list[viewed + 1]
-  const string = nextImage.split("/")
+next.addEventListener("mouseup", () => {
+  audio.pause();
+});
 
-  img.src = nextImage
+prev.addEventListener("mousedown", () => {
+  audio.play();
+});
 
-  description.textContent = string[string.length - 1]
-  viewed = viewed + 1
-})
+prev.addEventListener("mouseup", () => {
+  audio.pause();
+});
 
-prev.addEventListener('click', () => {
+next.addEventListener("click", () => {
+  const nextImage = list[viewed + 1];
+  const string = nextImage.split("/");
+
+  img.src = nextImage;
+
+  description.textContent = string[string.length - 1];
+  viewed = viewed + 1;
+});
+
+prev.addEventListener("click", () => {
   if (viewed <= 0) {
-    viewed = 1
+    viewed = 1;
   }
-  img.src = list[viewed - 1]
-  viewed = viewed - 1
-})
+  img.src = list[viewed - 1];
+  viewed = viewed - 1;
+});
